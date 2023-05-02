41e497da43e75e928e906eb263736f388b68f515
Dagurasu56 <douglas_search@hotmail.com>
2023-05-01 23:36:29 -0300

    Exceptions treatment

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -2,8 +2,9 @@ package br.ce.wcaquino.servicos;
 
 import static br.ce.wcaquino.utils.DataUtils.isMesmaData;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Usuario;
@@ -14,7 +15,7 @@ import org.junit.jupiter.api.Test;
 class LocacaoServiceTest {
 
   @Test
-  void teste() {
+  void testLocacaoFilme() throws Exception {
     // cenario
     var service = new LocacaoService();
     var usuario = new Usuario("Usuario 1");
@@ -25,8 +26,21 @@ class LocacaoServiceTest {
 
     // verificacao
     assertThat(locacao.getValor(), is(5.0));
-    assertThat(locacao.getValor(), is(not(6.0)));
     assertThat(isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
     assertThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
+  }
+
+  @Test
+  void testLocacaoFilmeSemEstoque() {
+    // cenario
+    var service = new LocacaoService();
+    var usuario = new Usuario("Usuario 1");
+    var filme = new Filme("Filme 1", 0, 5.0);
+
+    // acao
+    var exception = assertThrows(Exception.class, () -> service.alugarFilme(usuario, filme));
+
+    // verificacao
+    assertEquals("Filme sem estoque", exception.getMessage());
   }
 }
