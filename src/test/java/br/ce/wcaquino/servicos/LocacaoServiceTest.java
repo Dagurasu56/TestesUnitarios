8a3e00607b9b70c14553ed45edb3697b85de99a6
Dagurasu56 <douglas_search@hotmail.com>
2023-05-01 22:40:37 -0300

    Using assertions

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -1,29 +1,29 @@
 package br.ce.wcaquino.servicos;
 
 import static br.ce.wcaquino.utils.DataUtils.isMesmaData;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import br.ce.wcaquino.entidades.Filme;
-import br.ce.wcaquino.entidades.Locacao;
 import br.ce.wcaquino.entidades.Usuario;
 import br.ce.wcaquino.utils.DataUtils;
 import java.util.Date;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-public class LocacaoServiceTest {
+class LocacaoServiceTest {
 
     @Test
-    public void teste() {
+    void teste() {
         // cenario
         var service = new LocacaoService();
         var usuario = new Usuario("Usuario 1");
         var filme = new Filme("Filme 1", 2, 5.0);
 
         // acao
-        Locacao locacao = service.alugarFilme(usuario, filme);
+        var locacao = service.alugarFilme(usuario, filme);
 
         // verificacao
-        assertTrue(locacao.getValor() == 5.0);
+        assertEquals(5.0, locacao.getValor(), 0.01);
         assertTrue(isMesmaData(locacao.getDataLocacao(), new Date()));
         assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
     }
