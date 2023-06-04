5c7d6e06967fa3cf6ec0be02456c34820cfa8782
Dagurasu56 <douglas_search@hotmail.com>
2023-06-03 21:38:58 -0300

    Fixing JUnit conflicts

--- src/test/java/br/ce/wcaquino/servicos/OrdemTest.java
+++ src/test/java/br/ce/wcaquino/servicos/OrdemTest.java
@@ -2,24 +2,21 @@ package br.ce.wcaquino.servicos;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.runners.MethodSorters;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class OrdemTest {
 
   public static int contador = 0;
 
   @Test
-  @Order(1)
   public void inicia() {
     contador = 1;
   }
 
   @Test
-  @Order(2)
   public void verifica() {
     assertEquals(1, contador);
   }
--- src/test/java/br/ce/wcaquino/suites/SuiteExecucao.java
+++ src/test/java/br/ce/wcaquino/suites/SuiteExecucao.java
@@ -3,10 +3,8 @@ package br.ce.wcaquino.suites;
 import br.ce.wcaquino.servicos.CalculadoraTest;
 import br.ce.wcaquino.servicos.CalculoValorLocacaoTest;
 import br.ce.wcaquino.servicos.LocacaoServiceTest;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-@RunWith(Suite.class)
+//@RunWith(Suite.class)
 @SuiteClasses({CalculadoraTest.class, CalculoValorLocacaoTest.class, LocacaoServiceTest.class})
 public class SuiteExecucao {}
